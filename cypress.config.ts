f3c840c6c0efbab4f1a9ff50001670edba45aa38
Artem Shoiko <shoiko.a.s@gmail.com>
2023-12-06 23:14:21 +0200

    Lesson 17

--- cypress.config.ts
+++ cypress.config.ts
@@ -16,6 +16,13 @@ export default defineConfig({
   defaultCommandTimeout: 5000,   // default 4000
   requestTimeout: 10000,         // default 5000
   responseTimeout: 30000,         // default 30000
+  reporter: "mochawesome",
+  "reporterOptions": {
+    "overwrite": false,
+    "html": false,
+    "json": true,
+    reportDir: 'cypress/results'
+  },
   // watchForFileChanges: false,
   env: {
     POST: '4200',
